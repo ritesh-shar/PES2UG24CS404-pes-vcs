fdfa02aebb55dd9d7248034cf92ea06eb7225030
ritesh-shar <riteshksharma2006@gmail.com>
2026-04-21 06:35:14 +0000

    Serialized Binary Tree

--- tree.c
+++ tree.c
@@ -136,6 +136,16 @@ static int build_tree_recursive(IndexEntry *entries, int count, int start, int *
     Tree tree;
     tree.count = 0;
     int i = start;
+    void *data;
+    size_t len;
+    if (tree_serialize(&tree, &data, &len) != 0) return -1;
+    if (object_write(OBJ_TREE, data, len, id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+    if (consumed) *consumed = i - start;
     return 0;
 }
 
