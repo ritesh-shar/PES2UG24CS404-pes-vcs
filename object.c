443b3336bc52cf304049612cace888a643572734
ritesh-shar <riteshksharma2006@gmail.com>
2026-04-21 06:10:31 +0000

    Initialized ObjectType to String

--- object.c
+++ object.c
@@ -95,7 +95,16 @@ int object_exists(const ObjectID *id) {
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
     // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
+    const char *type_str = (type == OBJ_BLOB) ? "blob" : (type == OBJ_TREE ? "tree" : "commit");
+
+    char header[64];
+    int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1; // +1 for '\0'
+    size_t total_size = header_len + len;
+
+    uint8_t *full_obj = malloc(total_size);
+    if (!full_obj) return -1;
+    memcpy(full_obj, header, header_len);
+    memcpy(full_obj + header_len, data, len);
     return -1;
 }
 
